6df3c4ebbde8f98783d9e188426303829f6c1f43
hep-mh <gvrd9og8@anonaddy.me>
2021-07-08 23:00:23 +0200

    added exponential cutoff for photon photon scattering

--- acropolis/cascade.py
+++ acropolis/cascade.py
@@ -267,10 +267,11 @@ class _PhotonReactionWrapper(_ReactionWrapperScaffold):
 
     # PHOTON-PHOTON SCATTERING ################################################
     def _rate_photon_photon(self, E, T):
-        if E > me2/T:
-            return 0.
+        #if E > me2/T:
+        #    return 0.
+        expf = exp( -E*T/me2 )
 
-        return 0.151348 * (alpha**4.) * me * (E/me)**3. * (T/me)**6.
+        return 0.151348 * (alpha**4.) * me * (E/me)**3. * (T/me)**6. * expf
 
 
     # COMPTON SCATTERING ######################################################
@@ -341,7 +342,10 @@ class _PhotonReactionWrapper(_ReactionWrapperScaffold):
 
     # PHOTON-PHOTON SCATTERING ################################################
     def _kernel_photon_photon(self, E, Ep, T):
-        return 1112./(10125.*pi) * (alpha**4.)/(me**8.) * 8.*(pi**4.)*(T**6.)/63. * Ep**2. * ( 1. - E/Ep + (E/Ep)**2. )**2.
+        expf = exp( -E*T/me2 )
+
+        return 1112./(10125.*pi) * (alpha**4.)/(me**8.) * 8.*(pi**4.)*(T**6.)/63. \
+               * Ep**2. * ( 1. - E/Ep + (E/Ep)**2. )**2. *expf
 
 
     # COMPTON SCATTERING ######################################################
