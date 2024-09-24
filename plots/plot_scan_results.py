30fbef473f6a0f3f187a49bbf6bfebdc24533aca
hep-mh <mail@hep-mh.com>
2024-09-24 12:40:52 +0200

    updated plots

--- plots/plot_scan_results.py
+++ plots/plot_scan_results.py
@@ -15,7 +15,7 @@ data_dir = 'data/'
 # PLOT 1 ######################################################################
 # s-wave, a vs mchi ###########################################################
 _, ax1 = plot_scan_results(
-    data_dir + 'annih_swave_ee.dat', output_file=None,
+    data_dir + 'annih_swave_ee.dat', output_file=None, xc=1, yc=0,
     title=tex_title(b=0, tempkd=0, braa=0), labels=tex_labels('mchi', 'a')
 )
 
@@ -29,7 +29,7 @@ save_figure('annih_swave_ee.pdf')
 # PLOT 2 ######################################################################
 # p-wave, b vs mchi, tempkd = 1 MeV ###########################################
 plot_scan_results(
-    data_dir + 'annih_pwave_Tkd_1e+00MeV_ee.dat', output_file='annih_pwave_1e0MeV_ee.pdf',
+    data_dir + 'annih_pwave_Tkd_1e+00MeV_ee.dat', output_file='annih_pwave_1e0MeV_ee.pdf', xc=1, yc=0,
     title=tex_title(a=0, tempkd=1, braa=0), labels=tex_labels('mchi', 'b')
 )
 
@@ -38,7 +38,7 @@ plot_scan_results(
 # PLOT 3 ######################################################################
 # decay, n0a vs mphi, tau = 1e7 s #############################################
 plot_scan_results(
-    data_dir + 'decay_tau_1e+07s_aa.dat', output_file='decay_1e7s_aa.pdf',
+    data_dir + 'decay_tau_1e+07s_aa.dat', output_file='decay_1e7s_aa.pdf', xc=0, yc=1,
     title=tex_title(tau=1e7, temp0=10, braa=1), labels=tex_labels('mphi', 'n0a')
 )
 
@@ -47,7 +47,7 @@ plot_scan_results(
 # PLOT 4 ######################################################################
 # decay, n0a vs tau, mphi = 50 MeV ############################################
 _, ax4 = plot_scan_results(
-    data_dir + 'decay_mphi_5e+01MeV_aa.dat', output_file=None, fix_helium=True,
+    data_dir + 'decay_mphi_5e+01MeV_aa.dat', output_file=None, fix_helium=True, xc=1, yc=0,
     title=tex_title(mphi=50, temp0=10, braa=1), labels=tex_labels('tau', 'n0a')
 )
 
