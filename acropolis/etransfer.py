7bcdda7626ba1543ac245942d7d6ca2420653a4d
hep-mh <mail@hep-mh.com>
2024-09-26 17:12:50 +0200

    implemented inelastic pbg reactions

--- acropolis/etransfer.py
+++ acropolis/etransfer.py
@@ -1,16 +1,15 @@
 # math
-from math import log, exp
+from math import log, exp, sqrt
 # cmath
 from cmath import exp as cexp
 # numpy
 import numpy as np
 
+# hcascade
+from acropolis.hcascade import mass_dict, Projectiles, Targets, ParticleSpectrum
 # params
 from acropolis.params import pi
-# hcascade
-from acropolis.hcascade import mass_dict, Projectiles, Targets
-# jit
-from acropolis.jit import jit
+from acropolis.params import mpi0, mpic, Kt, mp, mn
 
 
 mb_to_iMeV2 = 2.5681899885e-06
@@ -38,7 +37,6 @@ def enforce_energy_conservation():
 
 
 # s in MeV²
-@jit
 def _bp(s):
     C = 10.94 * 1e-6 # 1/MeV²
     D = -0.09 * 1e-6 # 1/MeV²
@@ -48,7 +46,6 @@ def _bp(s):
 
 
 # s in MeV²
-@jit
 def _bm(s):
     C = 23.2 * 1e-6 # 1/MeV²
     D = 0.94 * 1e-6 # 1/MeV²
@@ -57,7 +54,6 @@ def _bm(s):
 
 
 # s in MeV²
-@jit
 def _Mp(s):
     A  = 41.8 * mb_to_iMeV2 # 1/MeV²
     B  = 0.68 * mb_to_iMeV2 # 1/MeV²
@@ -67,7 +63,6 @@ def _Mp(s):
 
 
 # s in MeV²
-@jit
 def _Mm(s):
     D  = -39e6*mb_to_iMeV2 # GeV^{-2al}
     al = 0.48
@@ -76,49 +71,156 @@ def _Mm(s):
 
 
 # s in MeV²
-@jit
-def Bsl(projectile, target, s):
+def _Bsl(projectile, target, s):
     if target == Targets.ALPHA:
         # The same expression is used for any type of projectile
         
         return 28 * 1e-6 # 1/MeV²
         #         | 1/GeV² --> 1/MeV²
     
-    bp = _bp(s)
-    bm = _bm(s)
+    if target == Targets.PROTON:
+        bp, bm = _bp(s), _bm(s)
+        Mp, Mm = _Mp(s), _Mm(s)
+        # -->
+        ReMp, ImMp = Mp.real, Mp.imag
+        ReMm, ImMm = Mm.real, Mm.imag
+        # -->
+        ReMp2, ImMp2 = ReMp**2., ImMp**2.
+        ReMm2, ImMm2 = ReMm**2., ImMm**2.
 
-    Mp = _Mp(s)
-    Mm = _Mm(s)
-    # -->
-    ReMp, ImMp = Mp.real, Mp.imag
-    ReMm, ImMm = Mm.real, Mm.imag
-    # -->
-    ReMp2, ImMp2 = ReMp**2., ImMp**2.
-    ReMm2, ImMm2 = ReMm**2., ImMm**2.
+        # Bsl = d ln(dsig / dt) /dt = (d²sig / dt²) / (dsig / dt) @ t = tref
+        def _Bsl(k):
+            tref = -0.02e6 # MeV²
 
-    # Bsl = d ln(dsig / dt) /dt = (d²sig / dt²) / (dsig / dt)
-    def _Bsl(k):
-        tref = -0.02e6 # MeV²
+            num = bm*exp(bm*tref)*ImMm2 + exp(bp*tref)*ImMp2*bp + bm*exp(bm*tref)*ReMm2 \
+                + bp*exp(bp*tref)*ReMp2 + (bm + bp)*exp(((bm + bp)*tref)/2.)*k*(ImMm*ImMp + ReMm*ReMp)
+            
+            den = exp(bm*tref)*(ImMm2 + ReMm2) + \
+                + 2.*exp(((bm + bp)*tref)/2.)*k*(ImMm*ImMp + ReMm*ReMp) + exp(bp*tref)*(ImMp2 + ReMp2)
 
-        num = bm*exp(bm*tref)*ImMm2 + exp(bp*tref)*ImMp2*bp + bm*exp(bm*tref)*ReMm2 \
-            + bp*exp(bp*tref)*ReMp2 + (bm + bp)*exp(((bm + bp)*tref)/2.)*k*(ImMm*ImMp + ReMm*ReMp)
+            return num/den # 1/MeV²
         
-        den = exp(bm*tref)*(ImMm2 + ReMm2) + \
-            + 2.*exp(((bm + bp)*tref)/2.)*k*(ImMm*ImMp + ReMm*ReMp) + exp(bp*tref)*(ImMp2 + ReMp2)
+        if projectile == Projectiles.PROTON \
+        or projectile == Projectiles.NEUTRON:
+            return _Bsl(-1) # 1/MeV²
 
-        #num = (ReMp*bp + k*ReMm*bm)*(ReMp + k*ReMm) + (ImMp*bp + k*ImMm*bm)*(ImMp + k*ImMm)
-        #den = (ReMp + k*ReMm)**2. + (ImMp + k*ImMm)**2.
-
-        return num/den # 1/MeV²
-    
-    # target == Targets.PROTON
-    if projectile == Projectiles.PROTON \
-    or projectile == Projectiles.NEUTRON:
-        return _Bsl(-1) # 1/MeV²
-
-    if projectile == Projectiles.ANTI_PROTON \
-    or projectile == Projectiles.ANTI_NEUTRON:
-        return _Bsl(+1) # 1/MeV²
+        if projectile == Projectiles.ANTI_PROTON \
+        or projectile == Projectiles.ANTI_NEUTRON:
+            return _Bsl(+1) # 1/MeV²
 
     # Invalid projectile or target
     return np.nan
+
+
+#####################################################################
+
+
+def _gcm(projectile, target, Ki):
+    mN, mA = mass_dict[projectile], mass_dict[target]
+
+    return ( (mN + mA) + Ki ) / sqrt( (mN + mA)**2. + 2.*mA*Ki )
+
+
+# TODO
+def _any_elastic(projectile, target, Ki, energy_grid):
+    if projectile == Projectiles.ANTI_PROTON \
+    or projectile == Projectiles.ANTI_NEUTRON:
+        raise ValueError(
+            "Scattering of anti-protons(neutrons) is currently not supported"
+        )
+    
+    # Initialize the spectrum
+    spectrum = ParticleSpectrum( energy_grid )
+    
+    # Calculate the maximal values of Kj' and t
+    mN, mA = mass_dict[projectile], mass_dict[target]
+    # -->
+    Kj_p_max = 2.*mA*Ki / ( (mN + mA)**2. + 2.*mA*Ki )
+    # -->
+    t_max = 2.*mA*Kj_p_max
+
+    # Calculate Bsl
+    s = _K_to_s(projectile, target, Ki)
+    # -->
+    Bsl = _Bsl(projectile, target, s)
+
+    # Calculate the prefactor of the distribution
+    pref = 1./( 1. - exp(-Bsl*tmax) )
+
+    return 0.
+
+
+# Reactions (i,p,2) and (i,p,3)
+#
+# convert_target = False:
+# p + p_bg -> p + p + pi0
+# n + p_bg -> n + p + pi0
+#
+# convert_target = True:
+# p + p_bg -> p + n + pi+
+# n + p_bg -> n + n + pi+
+def _proton_inelastic(energy_grid, projectile, Ki, convert_target):
+    if projectile == Projectiles.ANTI_PROTON \
+    or projectile == Projectiles.ANTI_NEUTRON:
+        raise ValueError(
+            "Scattering of anti-protons(neutrons) is currently not supported"
+        )
+    
+    # Initialize the spectrum
+    spectrum = ParticleSpectrum( energy_grid )
+
+    # Specify the target particle
+    target = Targets.PROTON
+
+    # Specify the daughter particle
+    daughter = Projectiles.PROTON if not convert_target else Projectiles.NEUTRON
+    # -->
+    (md, mpi) = (mp, mpi0) if not convert_target else (mn, mpic)
+
+    # Calculate the gamma factor for transforming
+    # from the com frame to the target rest frame
+    gcm = _gcm(projectile, target, Ki)
+
+    # Estimate the kinetic energy of the
+    # scattered projectile particle
+    Ki_p = .5*Ki
+
+    # Estimate the kinetic energy of the
+    # scattered target particle
+    Kj_p = gcm*Kt + (gcm - 1.)*md
+
+    # Estimate the kinetic energy of the
+    # produced neutral/charged pion
+    Kpi_p = gcm*Kt + (gcm - 1.)*mpi
+
+    # Ensure energy conservation
+    # TODO
+    #assert Ki + mp >= Ki_p + Kj_p + md + Kpi_p + mpi
+
+    spectrum.addp(projectile, 1, Ki_p)
+    spectrum.addp(daughter  , 1, Kj_p)
+    # pions can be ignored
+
+    return spectrum
+
+
+# Reaction (i,p,2)
+# p + p_bg -> p + p + pi0
+# n + p_bg -> n + p + pi0
+def proton_2(projectile, Ki, energy_grid):
+    return _proton_inelastic(energy_grid, projectile, Ki, convert_target=False)
+
+
+# Reaction (i,p,3)
+# p + p_bg -> p + n + pi+
+# n + p_bg -> n + n + pi+
+def proton_3(projectile, Ki, energy_grid):
+    return _proton_inelastic(energy_grid, projectile, Ki, convert_target=True)
+
+
+# Any reaction of the form
+# p + he4_bg -> p + X
+# n + he4_bg -> p + X
+# with X not including T/He3
+def _alpha_inel(projectile, Ki, daughter_nuclei, energy_grid):
+    pass
