2feb55b8c4276e1f88bc01ff15540e0799ae3b0f
hep-mh <mail@hep-mh.com>
2024-10-21 19:34:27 +0200

    formatting

--- acropolis/etransfer.py
+++ acropolis/etransfer.py
@@ -361,14 +361,16 @@ def _inelastic(spectrum, projectile, Ki, prob, bg, target, daughters, projectile
         dM -= md
     
     # DEBUG
-    print(f"Gamma factor      : {gcm:.7e}")
-    print(f"Mass difference   : {dM:.5e}MeV")
-    print(f"Energy balance    : {Ki:.3e}MeV (in) vs. {Ki_p+sum(Kj_p_L)-dM:.3e}MeV (out)")
-    print(f"Daughter particles:")
+    print(f"Gamma factor   : {gcm:+.5e}")
+    print(f"Mass difference: {dM:+.5e}MeV")
+    print(f"Energy balance : {Ki:+.5e}MeV (in)")
+    print(f"                 {Ki_p+sum(Kj_p_L)-dM:+.5e}MeV (out)")
+    print(f"FS particles   :")
     _l1 = [Ki_p, *Kj_p_L]
     _l2 = [projectile_remnant, *daughters]
     for _K, _d in zip(_l1, _l2):
-        print(f"  • {_d}: {_K:.3e}MeV")
+        print(f"  • {_d:<22}: {_K:.3e}MeV")
+    print(f"Spectrum log   :")
 
     # Ensure energy conservation
     # NOTE:
--- acropolis/particles.py
+++ acropolis/particles.py
@@ -201,7 +201,7 @@ class ParticleSpectrum(object):
 
     def add(self, particle, increment, K=0):
         # DEBUG
-        print(f"Adding {particle} ({increment:+.2e}) with kinetic energy {K:.2e}MeV")
+        print(f"  • Adding {particle:<17} ({increment:+.2e}) with kinetic energy {K:.2e}MeV")
 
         if is_projectile(particle):
             self._add_projectile(particle, increment, K)
