ba9856ca10a12623094732d952b23f50c66e8128
hep-mh <mail@hep-mh.com>
2024-12-13 19:38:53 +0100

    allow LogInterp to take negative y values

--- acropolis/utils.py
+++ acropolis/utils.py
@@ -12,13 +12,24 @@ from acropolis.jit import jit
 class LogInterp(object):
 
     def __init__(self, x_grid, y_grid, base=np.e, fill_value=None):
+        if not ( np.all(y_grid < 0) or np.all(y_grid > 0) ):
+            raise ValueError(
+                "The values in y_grid need to be either all positive or all negative."
+            )
+        if not np.all(x_grid > 0):
+            raise ValueError(
+                "The values in x_grid need to be all positive."
+            )
+        
+        self._sSign = 1 if y_grid[0] > 0 else -1
+
         self._sBase    = base
         self._sLogBase = log(self._sBase)
 
         self._sFillValue = fill_value
 
-        self._sXLog = np.log(x_grid)/self._sLogBase
-        self._sYLog = np.log(y_grid)/self._sLogBase
+        self._sXLog = np.log( np.abs(x_grid) )/self._sLogBase
+        self._sYLog = np.log( np.abs(y_grid) )/self._sLogBase
 
         self._sXminLog = self._sXLog[ 0]
         self._sXmaxLog = self._sXLog[-1]
@@ -68,7 +79,7 @@ class LogInterp(object):
         m = ( y2_log - y1_log )/( x2_log - x1_log )
         b = y2_log - m*x2_log
 
-        return pow( self._sBase, m*x_log + b )
+        return self._sSign * pow( self._sBase, m*x_log + b )
 
 
     def __call__(self, x):
