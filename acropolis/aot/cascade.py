fa47aa4170861f818d4ecc1789b1ecc62765e9ed
hep-mh <mail@hep-mh.com>
2025-03-07 17:31:26 +0100

    rename aot to _aot

--- acropolis/aot/cascade.py
+++ acropolis/aot/cascade.py
@@ -2,18 +2,18 @@
 from importlib import import_module
 
 # Load the compiled module dynamically
-aot = import_module(".compiled", package="acropolis.aot")
+_aot = import_module(".compiled", package="acropolis.aot")
 
 # Expose the compiled functions
-ph_rate_pair_creation_ae   = aot.ph_rate_pair_creation_ae
-ph_kernel_inverse_compton  = aot.ph_kernel_inverse_compton
-el_kernel_pair_creation_ae = aot.el_kernel_pair_creation_ae
-el_rate_inverse_compton    = aot.el_rate_inverse_compton
-el_kernel_inverse_compton  = aot.el_kernel_inverse_compton
+ph_rate_pair_creation_ae   = _aot.ph_rate_pair_creation_ae
+ph_kernel_inverse_compton  = _aot.ph_kernel_inverse_compton
+el_kernel_pair_creation_ae = _aot.el_kernel_pair_creation_ae
+el_rate_inverse_compton    = _aot.el_rate_inverse_compton
+el_kernel_inverse_compton  = _aot.el_kernel_inverse_compton
 
-dsdE_Z2                    = aot.dsdE_Z2
+dsdE_Z2                    = _aot.dsdE_Z2
 
-solve_cascade_equation     = aot.solve_cascade_equation
+solve_cascade_equation     = _aot.solve_cascade_equation
 
 # Specify which functions to export
 __all__ = [
